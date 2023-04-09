2d30ebe5e6ba554a8c49d38b562f3332a9fd9425
Frank K-a <33493184+newactual@users.noreply.github.com>
2023-04-09 20:44:43 +0000

    Initial commit

--- utils/event.js
+++ utils/event.js
@@ -1,18 +1,36 @@
-import { add } from "./math";
+import { add, multiply } from "./math";
 
 const eventHandler = () => {
 
     const addButton = document.getElementById('add');
+    const multiplyButton = document.getElementById('multiply');
     const number1 = document.getElementById('number-1');
     const number2 = document.getElementById('number-2');
     const result = document.getElementById('result');
+    const checkRequiredValues = (el1, el2) => {
+        if( (el1.value.length === 0) || (el2.value.length === 0)) {
+            return false;
+        }
+
+        return true;
+    }
     const updateDomWithAddedNumbers = () => {
-        result.innerHTML = 
-            // parseInt(number1.value, 10)+parseInt(number2.value,10);
-            add(number1.value,number2.value);
+        if( !checkRequiredValues(number1, number2) ) {
+            return result.innerHTML = "<span style='color: white;background-color: red;'>ERROR: Both inputs require a value</span>";
+        }
+
+        result.innerHTML = add(number1.value,number2.value) + " (result of addition)";
+    }
+    const updateDomWithMultipliedNumbers = () => {
+        if( !checkRequiredValues(number1, number2) ) {
+            return result.innerHTML = "<span style='color: white;background-color: red;'>ERROR: Both inputs require a value</span>";
+        }
+
+        result.innerHTML = multiply(number1.value, number2.value) + " (result of multiplication)";
     }
 
     addButton.addEventListener('click', updateDomWithAddedNumbers);
+    multiplyButton.addEventListener('click', updateDomWithMultipliedNumbers);
 
 }
 
--- utils/math.js
+++ utils/math.js
@@ -1,4 +1,5 @@
 const add = (n1,n2) => parseInt(n1, 10)+parseInt(n2, 10);
 
+const multiply = (n1,n2) => parseInt(n1) * parseInt(n2);
 
-export {add};
+export {add, multiply};
